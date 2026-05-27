d89b8fffa92b215ea1a7cd237713930144c12a2a
kevin <kevin@deepai.org>
2026-05-27 02:39:37 +0000

    Handle foreign process launch probes

--- bochs/cpu/proc_ctrl.cc
+++ bochs/cpu/proc_ctrl.cc
@@ -9738,7 +9738,8 @@ bool BX_CPU_C::handle_poly_memory_syscall(const char *arch_name, Bit32u syscall_
     return true;
   }
 
-  if (syscall_number == 236 ||
+  if (syscall_number == 220 || syscall_number == 221 ||
+      syscall_number == 236 ||
       syscall_number == 237 || syscall_number == 238 || syscall_number == 239 ||
       syscall_number == 277 || syscall_number == 280 || syscall_number == 282 ||
       (syscall_number >= 288 && syscall_number <= 290) ||
@@ -9749,7 +9750,9 @@ bool BX_CPU_C::handle_poly_memory_syscall(const char *arch_name, Bit32u syscall_
       syscall_number == 440 ||
       (syscall_number >= 444 && syscall_number <= 446) ||
       (syscall_number >= 448 && syscall_number <= 450)) {
-    const char *name = syscall_number == 236 ? "get_mempolicy" :
+    const char *name = syscall_number == 220 ? "clone" :
+      syscall_number == 221 ? "execve" :
+      syscall_number == 236 ? "get_mempolicy" :
       syscall_number == 237 ? "set_mempolicy" :
       syscall_number == 238 ? "migrate_pages" :
       syscall_number == 239 ? "move_pages" :
