bfbcb59a380c9fc00a957d40fede15b66f36ebc3
kevin <kevin@deepai.org>
2026-06-02 01:00:32 +0000

    Validate monitor packet target alignment

--- bochs/cpu/proc_ctrl.cc
+++ bochs/cpu/proc_ctrl.cc
@@ -1111,6 +1111,13 @@ static bool bx_poly_valid_trap_vector_target(Bit64u vector, Bit32u mode,
      bx_poly_frontend_target_aligned(mode, (bx_address) vector));
 }
 
+static bool bx_poly_valid_monitor_packet_target(Bit64u packet,
+  unsigned linaddr_width)
+{
+  return bx_poly_valid_control_address(packet, linaddr_width) &&
+    (packet == 0 || (packet & 0x7) == 0);
+}
+
 bool BX_CPU_C::bx_poly_target_has_landing_pad(unsigned seg, bx_address target,
   Bit32u mode)
 {
@@ -4408,7 +4415,7 @@ bool BX_CPU_C::import_poly_xsave_state(unsigned seg, bx_address base)
   }
   if (!bx_poly_valid_trap_vector_target(imported_trap_vector,
         (Bit32u) imported_trap_vector_mode, BX_CPU_THIS_PTR linaddr_width) ||
-      !bx_poly_valid_control_address(imported_monitor_packet,
+      !bx_poly_valid_monitor_packet_target(imported_monitor_packet,
         BX_CPU_THIS_PTR linaddr_width)) {
     BX_INFO(("poly_state_import: reject control addresses vector=%llx packet=%llx",
       (unsigned long long) imported_trap_vector,
@@ -11030,7 +11037,7 @@ bool BX_CPU_C::execute_poly_raw_aarch64(Bit32u insn, bx_address pc)
     Bit64u packet = 0;
     if (!read_poly_aarch64_reg(0, &packet))
       return false;
-    if (!bx_poly_valid_control_address(packet,
+    if (!bx_poly_valid_monitor_packet_target(packet,
           BX_CPU_THIS_PTR linaddr_width)) {
       write_poly_aarch64_reg(0, (Bit64u) (Bit64s) -22);
       RIP = next_rip;
@@ -12358,7 +12365,7 @@ bool BX_CPU_C::execute_poly_raw_riscv(Bit32u insn, bx_address pc)
     Bit64u packet = 0;
     if (!read_poly_riscv_reg(10, &packet))
       return false;
-    if (!bx_poly_valid_control_address(packet,
+    if (!bx_poly_valid_monitor_packet_target(packet,
           BX_CPU_THIS_PTR linaddr_width)) {
       write_poly_riscv_reg(10, (Bit64u) (Bit64s) -22);
       RIP = next_rip;
@@ -15486,9 +15493,9 @@ bool BX_CPP_AttrRegparmN(1) BX_CPU_C::handle_poly_opcode(bxInstruction_c *i)
         return true;
       }
       if (op == 0x6b) {
-        if (!bx_poly_valid_control_address(RAX,
+        if (!bx_poly_valid_monitor_packet_target(RAX,
               BX_CPU_THIS_PTR linaddr_width)) {
-          BX_INFO(("poly_ud: reject non-canonical monitor packet address=%llx",
+          BX_INFO(("poly_ud: reject monitor packet address=%llx",
             (unsigned long long) RAX));
           RAX = (Bit64u) (Bit64s) -22;
           RIP = next_rip;
